d94d3681f86f86a79c7f8aea6115da46ca3c64ef
Ignacio <ruiznaxo@gmail.com>
2020-12-17 15:17:51 -0300

    add sockets support

--- clases/server.ts
+++ clases/server.ts
@@ -4,6 +4,8 @@ import { SERVER_PORT } from '../global/enviroment';
 import socketIO from 'socket.io'
 import http from 'http'
 
+import * as socket from '../sockets/sockets'
+
 export default class Server {
 
     private static _intance: Server;
@@ -13,39 +15,46 @@ export default class Server {
 
     //encargada de eventos de los sockets
     public io: socketIO.Server;
-    private httpServer: http.Server; 
+    private httpServer: http.Server;
 
-    private constructor(){
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT
 
         //socket.io 2.1 necesita la config del servidor
         this.httpServer = new http.Server(this.app);
-        
-        //configuracion de sockets
+
+        //configuracion de sockets (io es el servidor de sockets)
         this.io = socketIO(this.httpServer);
 
         this.escucharSockets();
     }
 
-    public static get instance(){
+    public static get instance() {
         return this._intance || (this._intance = new this())
     }
 
 
-    private escucharSockets(){
+    private escucharSockets() {
         console.log("Escuchando sockets");
 
-        this.io.on('connection', cliente =>{
-            console.log("Cliente conecado");            
+        this.io.on('connection', cliente => {
+            console.log("Cliente conectado");
+
+            //mensaje
+            socket.mensaje(cliente, this.io)
+
+            //desconectar
+            socket.desconectar(cliente);
         })
-        
+
+
     }
 
 
     //Levantar Servidor
 
-    start(callback: any){
+    start(callback: any) {
         this.httpServer.listen(this.port, "", callback);
     }
 
